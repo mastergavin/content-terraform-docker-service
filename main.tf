d97a296dd7af9bee11e0aa8a5ebdeefea9091e75
mastergavin <gbellajr@gmail.com>
2019-07-29 13:42:38 -0400

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+resource "docker_service" "ghost-service" {
+  name = "ghost"
+
+  task_spec {
+    container_spec {
+      image = "${var.ghost_image}"
+
+      env = {
+        database__client               = "mysql"
+        database__connection__host     = "${var.mysql_network_alias}"
+        database__connection__user     = "${var.ghost_db_username}"
+        database__connection__password = "${var.mysql_root_password}"
+        database__connection__database = "${var.ghost_db_name}"
+      }
+    }
+    networks = ["${docker_network.public_bridge_network.name}", "${docker_network.private_bridge_network.name}"]
+  }
+
+  endpoint_spec {
+    ports {
+      target_port    = "2368"
+      published_port = "${var.ghost_ext_port}"
+    }
+  }
+}
+
 resource "docker_service" "mysql-service" {
   name = "${var.mysql_network_alias}"
 
@@ -19,28 +45,3 @@ resource "docker_service" "mysql-service" {
   }
 }
 
-resource "docker_service" "ghost-service" {
-  name = "ghost"
-
-  task_spec {
-    container_spec {
-      image = "${var.ghost_image}"
-
-      env = {
-         database__client               = "mysql"
-         database__connection__host     = "${var.mysql_network_alias}"
-         database__connection__user     = "${var.ghost_db_username}"
-         database__connection__password = "${var.mysql_root_password}"
-         database__connection__database = "${var.ghost_db_name}"
-      }
-    }
-    networks = ["${docker_network.public_bridge_network.name}", "${docker_network.private_bridge_network.name}"]
-  }
-
-  endpoint_spec {
-    ports {
-      target_port    = "2368"
-      published_port = "${var.ghost_ext_port}"
-    }
-  }
-}
